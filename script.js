0ce695c3e242f2d8991dce6322a2c9f0f8f681c3
ashish__12 <ashish.suresh@finalyca.com>
2023-08-07 00:59:42 +0530

    finally completed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // slecting burger icon 
 const burger = document.querySelector('.burger')
 console.log(burger);
-const header = document.querySelector('.right-sec')
+const header = document.querySelector('header')
 
 burger.addEventListener('click', ()=>{
     // console.log();
